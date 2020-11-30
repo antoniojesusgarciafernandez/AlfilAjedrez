e824ae101d88d52b5c4374ccc143c1f995781e48
Terry1504 <Terry1504@DESKTOP-03Q41G2>
2020-11-30 19:20:04 +0100

    Creado constructor copia

--- src/main/java/org/iesalandaluz/programacion/alfilajedrez/Posicion.java
+++ src/main/java/org/iesalandaluz/programacion/alfilajedrez/Posicion.java
@@ -3,51 +3,48 @@ package org.iesalandaluz.programacion.alfilajedrez;
 public class Posicion {
 	private int fila;
 	private char columna;
-	
-	//constructor
-	
-		public Posicion(int fila, char columna) {
-			if ((fila<1 || fila>8) && (columna<'a' || columna> 'h') ) {
-				throw new IllegalArgumentException("ERROR: Fila o columna no son válidos.");
-			} else {
-				setFila(fila);
-				setColumna(columna);
-			}
-			
-			
-			
+
+	// constructor
+
+	public Posicion(int fila, char columna) {
+		if ((fila < 1 || fila > 8) && (columna < 'a' || columna > 'h')) {
+			throw new IllegalArgumentException("ERROR: Fila o columna no son válidos.");
+		} else {
+			setFila(fila);
+			setColumna(columna);
 		}
-		
-		
-	
+
+	}
+
+	public Posicion(Posicion posicion) {
+		this.fila = posicion.fila;
+		this.columna = posicion.columna;
+	}
+
 	public int getFila() {
 		return fila;
 	}
-	
-	
+
 	private void setFila(int fila) {
-		
-		if (fila<1 || fila>8) {
+
+		if (fila < 1 || fila > 8) {
 			throw new IllegalArgumentException("ERROR: Fila no válida.");
 		}
-		
+
 		this.fila = fila;
 	}
-	
-	
+
 	public char getColumna() {
 		return columna;
 	}
-	
-	
+
 	private void setColumna(char columna) {
-		
-		if (columna<'a' || columna> 'h') {
+
+		if (columna < 'a' || columna > 'h') {
 			throw new IllegalArgumentException("ERROR: Columna no válida.");
 		}
-		
+
 		this.columna = columna;
-		
-		
+
 	}
 }
