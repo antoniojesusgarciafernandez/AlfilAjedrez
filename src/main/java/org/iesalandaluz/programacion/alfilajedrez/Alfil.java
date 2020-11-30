867fee320b1be690baeb5ca6a80509947a180691
Terry1504 <Terry1504@DESKTOP-03Q41G2>
2020-11-30 19:47:36 +0100

    Creados los métodos get y set para cada atributo

--- src/main/java/org/iesalandaluz/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandaluz/programacion/alfilajedrez/Alfil.java
@@ -4,10 +4,31 @@ public class Alfil {
 
 	private Color color;
 	private Posicion posicion;
-	
-	
 
-	
+	public Color getColor() {
+		return color;
+	}
 
-	
+	public void setColor(Color color) {
+		try {
+			this.color = color;
+		} catch (NullPointerException | IllegalArgumentException e) {
+			System.err.println("ERROR: El color no es válido.");
+		}
+
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(Posicion posicion) {
+		try {
+			this.posicion = posicion;
+		} catch (NullPointerException | IllegalArgumentException e) {
+			System.err.println("ERROR: La posicion no es válida.");
+		}
+		
+	}
+
 }
